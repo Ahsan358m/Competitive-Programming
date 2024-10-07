0de5c1e9e9d7137f556575a20bb73722c7d6023d
Ahsan358m <85702939+Ahsan358m@users.noreply.github.com>
2024-10-07 11:10:40 +0000

    BergMal("Kattis")

---
+++ BergMal.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class BergMal{
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+
+        String input = br.readLine().trim();
+        pw.println(input);
+        
+        pw.flush();
+        br.close();
+        pw.close();
+    }
+}
